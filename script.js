b2491b2ffa693efe1dcace320ac24888c93db360
Pauline_13 <pauline.tincq@yahoo.fr>
2025-05-13 12:00:17 +0200

    compréhension code JS

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// URL de l'API ↓ 
-const UrlApi = "https://randomuser.me/api/?results=50"
 
 // Remplace cette URL par celle de ton API réelle
 const API_URL = "https://randomuser.me/api/?results=8"; // ou le lien que tu as reçu
@@ -21,29 +19,32 @@ fetch(API_URL)
     donateurs.forEach(donateur => {
       // Création de la carte
       const figure = document.createElement("figure");
-      figure.classList.add("card_section");
+      // figure.classList.add("card_section");
 
-      const h2 = document.createElement("h2");
-      h2.classList.add("title_card");
-      h2.textContent = `${donateur.name.last} ${donateur.name.first}`;
+     const h2Last = document.createElement("h2");
+      h2Last.classList.add("last-name_card");
+      h2Last.textContent = donateur.name.last;
+
+        const h2First = document.createElement("h2");
+      h2First.classList.add("first-name_card");
+      h2First.textContent = donateur.name.first;
 
       const img = document.createElement("img");
-      img.classList.add("img_card");
+      // img.classList.add("img_card");
       img.src = donateur.picture.large;
-      img.alt = `Photo de ${donateur.name.first}`;
 
       const p = document.createElement("p");
-      p.classList.add("amount_card");
+      // p.classList.add("amount_card");
 
       // Montant aléatoire entre 10 et 100 €
-      const montant = (Math.random() * 90 + 10).toFixed(2);
+      const montant = (Math.random() * 90 + 10).toFixed(0);
       p.textContent = `Montant : ${montant} €`;
 
       // Assemblage
-      figure.appendChild(h2);
+      figure.appendChild(h2Last);
+      figure.appendChild(h2First);
       figure.appendChild(img);
       figure.appendChild(p);
-
       section.appendChild(figure);
     });
   })
